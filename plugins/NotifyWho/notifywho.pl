4d7bdc3f8c444c6cb6be3aa1bcd0873cbe4af6ba
Dan Wolfgang <dan@uinnovations.com>
2013-03-22 10:26:09 -0400

    Bump to version 2.0.4.

--- plugins/NotifyWho/notifywho.pl
+++ plugins/NotifyWho/notifywho.pl
@@ -15,7 +15,7 @@ use MT::Mail;
 use NotifyWho::Notification;
 
 # Public version number
-our $VERSION = "2.0.3";
+our $VERSION = "2.0.4";
 
 # Development revision number
 #our $Revision = ('$Revision: 504 $ ' =~ /(\d+)/);
